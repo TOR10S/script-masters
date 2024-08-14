3e52b862babde92ed3ad6a6b0d95500a1ae70fe8
TOR10S <andriivoro@gmail.com>
2024-08-14 23:31:18 +0300

    prep

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+import Swiper from 'swiper';
+import 'swiper/css';
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import * as aboutMe from './js/about-me';
+import * as faq from './js/faq';
+import * as footer from './js/footer';
+import * as header from './js/header';
+import * as projects from './js/projects';
+import * as reviews from './js/reviews';
