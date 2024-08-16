dca0174fb53f4d4585b021fcc7473cef8f778ac0
NikolayPutyata <nikolayputyata@gmail.com>
2024-08-16 16:25:32 +0300

    settings js, css, html

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -3,16 +3,14 @@ import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import Swiper from 'swiper';
-import 'swiper/css/bundle';
+import Swiper from 'swiper/bundle';
+import 'swiper/css';
 
 const domElements = {
   reviewsSection: document.querySelector('.reviews-section'),
   cardsUl: document.querySelector('.reviews-cards'),
-  leftArrow: document.querySelector('.left-arrow'),
-  rightArrow: document.querySelector('.right-arrow'),
 };
-const { cardsUl, leftArrow, rightArrow, reviewsSection } = domElements;
+const { cardsUl, reviewsSection } = domElements;
 
 export async function downloadCommentsFromServer() {
   cardsUl.innerHTML = '';
@@ -28,7 +26,7 @@ export async function downloadCommentsFromServer() {
     } else {
       const dataToHtml = responseData
         .map(comment => {
-          return `<li class="reviews-cards-li"><svg class="icon" xmlns="http://www.w3.org/2000/svg">
+          return `<li class="swiper-slide reviews-cards-li"><svg class="icon" xmlns="http://www.w3.org/2000/svg">
           <image href="${comment.avatar_url}" width="48" height="48" />
         </svg><h4>${comment.author}</h4><p class="card-comment">${comment.review}</p></li>`;
         })
@@ -43,28 +41,23 @@ export async function downloadCommentsFromServer() {
       message: `${err}`,
     });
   } finally {
-    // const swiper = new Swiper('.swiper-container', {
-    //   // Default parameters
-    //   slidesPerView: 1,
-    //   spaceBetween: 10,
-    //   // Responsive breakpoints
-    //   breakpoints: {
-    //     // when window width is >= 320px
-    //     320: {
-    //       slidesPerView: 2,
-    //       spaceBetween: 20,
-    //     },
-    //     // when window width is >= 480px
-    //     480: {
-    //       slidesPerView: 3,
-    //       spaceBetween: 30,
-    //     },
-    //     // when window width is >= 640px
-    //     640: {
-    //       slidesPerView: 4,
-    //       spaceBetween: 40,
-    //     },
-    //   },
-    // });
+    new Swiper('.swiper', {
+      slidesPerView: 1,
+      navigation: {
+        nextEl: '.right-arrow',
+        prevEl: '.left-arrow',
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1440: {
+          slidesPerView: 4,
+        },
+      },
+    });
   }
 }
