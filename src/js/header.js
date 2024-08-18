284e01b58d2b6752d16ba0c967219e750b2889ab
MariaPanchenko <marysa2502@gmail.com>
2024-08-18 23:43:21 +0300

    header changes

--- src/js/header.js
+++ src/js/header.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector('.header-menu-btn');
 const closeBtn = document.querySelector('.header-close');
 const modalWindow = document.querySelector('.modal-window');
 const mobileLink = document.querySelector('.mobile-link');
+const navList = document.querySelector('.header-menu-items');
 
 if (menuBtn && closeBtn && modalWindow && mobileLink) {
   let isMenuOpen = false;
@@ -24,7 +25,14 @@ if (menuBtn && closeBtn && modalWindow && mobileLink) {
     toggleModalWindowClass();
   }
 
+  function handleNavList(e) {
+    if (e.target.nodeName === 'A' && isMenuOpen) {
+      handleCloseMobileMenu();
+    }
+  }
+
   menuBtn.addEventListener('click', handleMenuBtnClick);
   closeBtn.addEventListener('click', handleCloseMobileMenu);
   mobileLink.addEventListener('click', handleCloseMobileMenu);
+  navList.addEventListener('click', handleNavList);
 }
