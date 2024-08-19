f2eddd271fb9e5b947e690c053226bd74fc319d2
Iryna Liskevych <160422110+Ira0000@users.noreply.github.com>
2024-08-19 13:38:54 +0200

    about-me change swiper container

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -10,13 +10,32 @@ new Accordion('.about-info-list', {elementClass: 'info-item',
     panelClass: 'info-item-panel',
 })
 
-const swiper = new Swiper(".tec-swiper", {
+const swiper = new Swiper('.tec-swiper-container', {
+  direction: 'horizontal',
   loop: true,
-  slidesPerView: 1,
-  spaceBetween: 0,
-  navigation: {
-    nextEl: ".arrow-btn-next",
+  effect: 'slide',
+  breakpointsBase: 'container',
+  slidesPerView: 2,
+  // slidesPerGroup: 2,
+  // slidesPerGroupSkip: 1,
+  // centeredSlides: false,
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 5,
+    },
   },
+  navigation: {
+    nextEl: '.about-button-next',
+  },
+  keyboard: {
+        enabled: true,
+      },
 });
 
 
